5817e64f773461ef1c6805d815367e7029752bfc
ianxoquant <85585921+ianxoquant@users.noreply.github.com>
2021-07-03 16:31:22 -0400

    Update hullwhite.py
    swaption helper functions

--- hullwhite.py
+++ hullwhite.py
@@ -2,6 +2,8 @@ import numpy as np
 import unittest
 
 """Hull White cap and swaption calculators"""
+"""Hull Options, Futures and Other Derivatives p575 -
+for the formulae for BtTdt and AtTdt"""
 """After Brigo/Mercurio Second Edition pp71-77"""
 
 
@@ -18,6 +20,24 @@ def AtT(a, v, t, T, DFt, DFT, f):
     return(AtT_value)
 
 
+def AtTdt(a, v, t, T, dt, DFt, DFT, DFdt):
+    """TODO: Add assertion on dt > 0.0"""
+    BtT_value = BtT(a, t, T)
+    Btdt_value = BtT(a, t, t + dt)
+    AtT_value = DFT/DFt * np.exp(-(BtT_value/Btdt_value)*np.log(DFdt/DFt))
+    # print(BtT_value, Btdt_value, AtT_value)
+
+    if a == 0.0:  # TODO should be an eps test?
+        AtT_value = AtT_value * np.exp(-(v*v)/(2.0) *
+                                       (t)
+                                       * BtT_value * (BtT_value - Btdt_value))
+    else:
+        AtT_value = AtT_value * np.exp(-(v*v)/(4.0*a) *
+                                       (1.0 - np.exp(-2.0*a*t))
+                                       * BtT_value * (BtT_value - Btdt_value))
+    return(AtT_value)
+
+
 def BtT(a, t, T):
     if a == 0.0:  # TODO should be an eps test?
         return(T-t)
@@ -27,7 +47,7 @@ def BtT(a, t, T):
 
 
 def BtTdt(a, t, T, dt):
-    return(dt*BtT(a, t, T)/BtT(a, t, T+dt))
+    return(dt*BtT(a, t, T)/BtT(a, t, t+dt))
 
 
 def PtTr(a, v, t, T, DFt, DFT, f, r):
@@ -36,6 +56,13 @@ def PtTr(a, v, t, T, DFt, DFT, f, r):
 
     PtTr_value = AtT_value * np.exp(-BtT_value*r)
     return(PtTr_value)
+
+
+def PtTdtr(a, v, t, T, dt, DFt, DFT, DFdt, r):
+    BtTdr_value = BtTdt(a, t, T, dt)
+    AtTdt_value = AtTdt(a, v, t, T, dt, DFt, DFT, DFdt)
+
+    return(AtTdt_value*np.exp(-BtTdr_value*r))
 
 
 def VtT(a, v, t, T):
@@ -47,7 +74,7 @@ def VtT(a, v, t, T):
 
 def VtTwo(a, v, t, T):
     """Price volatility of a zero coupon bond"""
-    """Long hand"""
+    """Long hand, doesn't trap a=0.0"""
     return(v/a * (1-np.exp(-a*(T-t))) * np.sqrt((1-np.exp(-2*a*t))/(2*a)))
 
 
@@ -70,11 +97,28 @@ class TestHullWhiteFunctions(unittest.TestCase):
         self.assertAlmostEqual(BtT(0.015, 5.0, 7.0),
                                1.9702977634327898, 9)
 
+    def test_BtTdt(self):
+        self.assertAlmostEqual(BtTdt(0.1, 0.25, 1.25, 0.5),
+                               0.9756147122503577, 9)
+
     def test_VtT(self):
         self.assertAlmostEqual(VtT(0.1, 0.02, 3, 5),
                                0.054452531346, 9)
         self.assertAlmostEqual(VtT(0.1, 0.015, 0.25, 1.25),
                                VtTwo(0.1, 0.015, 0.25, 1.25), 9)
+
+    def test_Swaption(self):
+        reversion = 0.1
+        volatility = 0.015
+        dfs = np.exp([-0.095*0.25, -0.105*0.75, -0.115*1.25])
+        self.assertAlmostEqual(AtTdt(reversion, volatility,
+                                     0.25, 1.25, 0.5,
+                                     dfs[0], dfs[2], dfs[1]),
+                               0.9873857422763, 9)
+        self.assertAlmostEqual(106*PtTdtr(reversion, volatility,
+                                          0.25, 1.25, 0.5,
+                                          dfs[0], dfs[2], dfs[1], .10675),
+                               94.3109904581068, 9)
 
 
 if __name__ == '__main__':
